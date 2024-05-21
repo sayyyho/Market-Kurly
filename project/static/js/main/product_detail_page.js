8d222b091ce21b748a0ad3521a73336a0a2c2b39
sayyyho <323psh@naver.com>
2024-05-21 23:02:41 +0900

    Feat: 상품가격 계산기능 구현

--- project/static/js/main/product_detail_page.js
+++ project/static/js/main/product_detail_page.js
@@ -1,1 +1,24 @@
 /* product detail page - JS */
+
+const increment = document.querySelector(".increment");
+const decrement = document.querySelector(".decrement");
+const count = document.querySelector(".total-count");
+const price = Number(
+  document.querySelector(".default-price").textContent.split(" ")[0]
+);
+const totalPrice = document.querySelector(".final-price");
+let countNum = 1;
+
+increment.addEventListener("click", () => {
+  countNum += 1;
+  count.textContent = countNum;
+  totalPrice.textContent = `${countNum * price} 원`;
+});
+
+decrement.addEventListener("click", () => {
+  if (countNum > 1) {
+    countNum -= 1;
+    count.textContent = countNum;
+    totalPrice.textContent = `${countNum * price} 원`;
+  }
+});
