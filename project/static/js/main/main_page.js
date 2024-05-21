824dc523a52d49a67ad26cff9a1ffc1815775a46
sayyyho <323psh@naver.com>
2024-05-21 16:14:18 +0900

    Feat: 신상품 패이지 구현 완료

--- project/static/js/main/main_page.js
+++ project/static/js/main/main_page.js
@@ -4,27 +4,72 @@ const categoryItem = document.querySelectorAll(
   '.category-item input[type="checkbox"]'
 );
 
-const selectBox = document.querySelector(".category-selection-view");
+const selectedCategory = document.querySelectorAll(".category-selection-item");
+const selectionView = document.querySelector(".category-selection-view");
+const clearBtn = document.querySelector("#clear");
+const sortingCount = document.querySelector(".sorting-count");
+let countHidden = 0;
+let countproduct = 0;
 
-const selectedItem = categoryItem.forEach((checkbox) => {
-  checkbox.addEventListener("change", (event) => {
-    const checkbox = event.target;
-    const label = checkbox.nextElementSibling;
-    const isChecked = checkbox.checked;
-    const itemClass = "selected-" + checkbox.id;
-
-    if (isChecked) {
-      console.log(`Checked: ${label.textContent}`);
-      const item = document.createElement("div");
-      item.textContent = label.textContent;
-      item.classList.add(itemClass);
-      selectBox.appendChild(item);
-    } else {
-      const itemToRemove = selectBox.querySelector("." + itemClass);
-      if (itemToRemove) {
-        selectBox.removeChild(itemToRemove);
-      }
-      selectBox.removeChild();
+selectedCategory.forEach((category, index) => {
+  if (category.classList.contains("hidden-class")) {
+    countHidden += 1;
+  }
+  const closeButton = category.querySelector("img");
+  closeButton.addEventListener("click", () => {
+    const bindingID = index + 1;
+    const mappingCheck = document.querySelector(`#check-${bindingID}`);
+    const mappingCount = document.querySelector(`#count-${bindingID}`);
+    mappingCheck.checked = false;
+    countproduct -= Number(mappingCount.textContent);
+    sortingCount.textContent = `총 ${countproduct}건`;
+    category.classList.add("hidden-class");
+    countHidden += 1;
+    if (countHidden === 3) {
+      selectionView.classList.add("hidden-class");
+      sortingCount.textContent = "총 508건";
+      countproduct = 0;
     }
   });
+});
+
+const selectedItem = categoryItem.forEach((checkbox, index) => {
+  checkbox.addEventListener("change", (event) => {
+    const isChecked = event.target.checked;
+    const bindingID = index + 1;
+    const mappingSelection = document.querySelector(`#selection-${bindingID}`);
+    const mappingCount = document.querySelector(`#count-${bindingID}`);
+    if (isChecked) {
+      if (countHidden === 3) {
+        selectionView.classList.remove("hidden-class");
+      }
+      countHidden -= 1;
+      mappingSelection.classList.remove("hidden-class");
+      countproduct += Number(mappingCount.textContent);
+      sortingCount.textContent = `총 ${countproduct}건`;
+    } else {
+      mappingSelection.classList.add("hidden-class");
+      countHidden += 1;
+      countproduct -= Number(mappingCount.textContent);
+      sortingCount.textContent = `총 ${countproduct}건`;
+      if (countHidden === 3) {
+        selectionView.classList.add("hidden-class");
+        sortingCount.textContent = "총 508건";
+        countproduct = 0;
+      }
+    }
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  categoryItem.forEach((item, index) => {
+    item.checked = false;
+    const bindingID = `#selection-${index + 1}`;
+    const mappingSelection = document.querySelector(bindingID);
+    mappingSelection.classList.add("hidden-class");
+  });
+  selectionView.classList.add("hidden-class");
+  countHidden = 3;
+  countproduct = 0;
+  sortingCount.textContent = "총 508건";
 });
